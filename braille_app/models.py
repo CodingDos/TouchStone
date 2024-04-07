de116ee5c53e292901850c34c0b587a0192c770e
Laband <lukeabandeen@gmail.com>
2024-04-07 12:12:50 -0400

    just set up the very baselevel framework will continue to add more, just wanted to save this first. just set up project and an app connected the two, downloaded the base requirements and set up the create-database doc and created the most basic of views'

--- braille_app/models.py
+++ braille_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Home(APIView):
+  def get(self, request):
+    content = {'message': 'Welcome to the cat-collector api home route!'}
+    return Response(content)
