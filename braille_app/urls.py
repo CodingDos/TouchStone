56a044a659d767f340cabf767eb64001d98a9ca1
Laband <lukeabandeen@gmail.com>
2024-04-16 10:16:14 -0400

    added some paths and views to braille

--- braille_app/urls.py
+++ braille_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import Home
+from .views import Home, BrailleList, BrailleDetail
 
 urlpatterns = [
         path('', Home.as_view(), name='home'),
+        path('braille/', BrailleList.as_view(), name='braille-list'),
+        path('braille/<str:info>/', BrailleDetail.as_view(), name='braille-detail'),
         ]
--- braille_app/views.py
+++ braille_app/views.py
@@ -1,7 +1,38 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .serializers import BrailleSerializer, WordSerializer, PhraseSerializer
+from .models import Braille, Words, Phrases
+from rest_framework import generics, filters, permissions
+from django.db.models import Q
 
 class Home(APIView):
     def get(self, request):
-        content = {'message': 'Welcome to the cat-collector api home route!'}
+        content = {'message': 'Welcome to the TouchStone api home route!'}
         return Response(content)
+
+
+class BrailleList(generics.ListAPIView):
+    queryset = Braille.objects.all()
+    serializer_class = BrailleSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['english', 'braille', 'binary']
+    ordering_fields = ['english', 'binary']
+
+
+class BrailleDetail(generics.RetrieveAPIView):
+    serializer_class = BrailleSerializer
+
+    def get_queryset(self):
+        info = self.kwargs['info']
+        binaryinfo = 0
+        if info.isdigit() :
+            binaryinfo = int(info)
+        return Braille.objects.filter(Q(english=info) | Q(braille=info) | Q(binary=binaryinfo))
+
+    def get_object(self):
+        queryset = self.get_queryset()
+        obj = generics.get_object_or_404(queryset)
+        self.check_object_permissions(self.request, obj)
+        return obj
+
+
