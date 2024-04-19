6ffe9d7eaea3007866cc267ff944f8fa2fdff207
Grace Clower <clower.grace@gmail.com>
2024-04-19 12:08:39 -0400

    started refining directory card for specifics

--- Frontend/src/Components/DirectoryCard/DirectoryCard.jsx
+++ Frontend/src/Components/DirectoryCard/DirectoryCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useState, useEffect } from "react";
 import "./DirectoryCard.css";
 
-function DirectoryCard() {
+function DirectoryCard(props) {
 
   
 
@@ -11,19 +11,19 @@ function DirectoryCard() {
     <div className="dircard">
       <div className="dircard-examples">
         <div className="dircard-example1">
-          <h3>Aa</h3>
+          <h3>{props.example1}</h3>
           <img className="dircard-braille-img" src="src/assets/braille/braille_a_100000.png" />
         </div>
         <div className="dircard-example2">
-          <h3>Bb</h3>
+          <h3>{props.example2}</h3>
           <img className="dircard-braille-img" src="src/assets/braille/braille_b_110000.png" />
         </div>
         <div className="dircard-example3">
-          <h3>Cc</h3>
+          <h3>{props.example3}</h3>
           <img className="dircard-braille-img" src="src/assets/braille/braille_c_100100.png" />
         </div>
       </div>
-      <h4>Alphabet</h4>
+      <h4>{props.title}</h4>
     </div>
   );
 }
