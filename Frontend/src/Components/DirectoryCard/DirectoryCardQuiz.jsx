186a5b8fe5384b82baec992f79d3784b6692fd5e
Grace Clower <clower.grace@gmail.com>
2024-04-22 11:18:59 -0400

    creaded phrases and quiz main dir cards

---
+++ Frontend/src/Components/DirectoryCard/DirectoryCardQuiz.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./DirectoryCard.css"
+
+function DirectoryCardQuiz() {
+  return (
+    <div className="dircard">
+    <div className="dircard-examples">
+      <div className="dircard-quiz">
+        <img className="dircard-braille-img" src="src/assets/braille/braille_a_100000.png" />
+        <i class="fa fa-long-arrow-right"></i>
+        <h1 className='question'>?</h1>
+      </div>
+    </div>
+    <h4>Quiz</h4>
+  </div>
+  )
+}
+
+export default DirectoryCardQuiz
