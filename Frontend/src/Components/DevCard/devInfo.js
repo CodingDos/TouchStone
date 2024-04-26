f4fad31d572adb5647e677d00f0b618709be2c5a
John Lopez <john.lopez3695@gmail.com>
2024-04-26 17:26:02 -0400

    modified gitignore to include developers information

---
+++ Frontend/src/Components/DevCard/devInfo.js
@@ -0,0 +1,66 @@
+import johnImg from "../../assets/devs/john-img.png";
+import stevenImg from "../../assets/devs/steven-img.png";
+import noshinImg from "../../assets/devs/noshin-img.png";
+import karinaImg from "../../assets/devs/karina-img.png";
+import xuemeiImg from "../../assets/devs/xuemei-img.png";
+import graceImg from "../../assets/devs/grace-img.png";
+import lukeImg from "../../assets/devs/luke-img.png";
+
+const developers = [
+  {
+    name: "John Lopez",
+    img: johnImg,
+    role: "Developer",
+    specialty: "AI & Front End",
+    github: "https://github.com/JLopez0001",
+    linkedIn: "https://www.linkedin.com/in/john-lopez01/",
+  },
+  {
+    name: "Steven Villacis ",
+    img: stevenImg,
+    role: "Developer",
+    specialty: "AI & Front End",
+    github: "https://github.com/CodingDos",
+    linkedIn: "https://www.linkedin.com/in/steven-villacis/",
+  },
+  {
+    name: "Noshin Chowdhury",
+    img: noshinImg,
+    role: "Developer",
+    specialty: "AI & Front End",
+    github: "https://github.com/noshinc346",
+  },
+  {
+    name: "Karina Sandoval Alvarez",
+    img: karinaImg,
+    role: "UX/UI Designer",
+    specialty: "Design",
+    website: "https://karinasandovalalvarez.myportfolio.com",
+    linkedIn: "https://www.linkedin.com/in/karina-sandoval-alvarez-66a97a2ba/",
+  },
+  {
+    name: "Xuemei O'Boyle",
+    img: xuemeiImg,
+    role: "UX/UI Designer",
+    specialty: "Design",
+    linkedIn: "https://www.linkedin.com/in/xuemei-o-3852a71b1/",
+  },
+  {
+    name: "Grace Clower",
+    img: graceImg,
+    role: "Developer",
+    specialty: "Front End",
+    github: "https://github.com/geclower",
+    linkedIn: "https://www.linkedin.com/in/grace-clower/",
+  },
+  {
+    name: "Luke Bandeen",
+    img: lukeImg,
+    role: "Developer",
+    specialty: "Back End & Front End",
+    github: "https://github.com/labandor",
+    linkedIn: "https://www.linkedin.com/in/luke-bandeen/",
+  },
+];
+
+export default developers;
