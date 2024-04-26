44d73713d6a542675f9776468a4c36dc1a45d9e8
John Lopez <john.lopez3695@gmail.com>
2024-04-26 15:04:49 -0400

    added props to change the style of the title and image of the card component without having to affect other card components

--- Frontend/src/Components/Card/Card.jsx
+++ Frontend/src/Components/Card/Card.jsx
@@ -1,23 +1,31 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import "./Card.css"
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./Card.css";
 
-function Card({height, width, title, refimg, brailleimg, matched}) {  
-
+function Card({
+  height,
+  width,
+  title,
+  refimg,
+  brailleimg,
+  titleStyle,
+  imgStyle,
+}) {
   return (
-    <div 
-    className={`card ${matched ? 'matched' : ''}`}
-    // className='card'
-    style={{
-      'width': width,
-      'height': height
-    }}
+    <div
+      className="card"
+      style={{
+        width: width,
+        height: height,
+      }}
     >
-      <h3 className='card-title'>{title}</h3>
-      <img className="card-ref-img" src={refimg} />
-      <img className="card-braille-img" src={brailleimg} /> 
+      <h3 className="card-title" style={titleStyle}>
+        {title}
+      </h3>
+      <img className="card-ref-img" src={refimg} style={imgStyle} />
+      <img className="card-braille-img" src={brailleimg} style={imgStyle} />
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
