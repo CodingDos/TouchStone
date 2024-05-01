22e6422c64a4b694a5e809b6f809073aee3658ca
Noshinc346 <noshin.chowdhury31@gmail.com>
2024-05-01 16:56:16 -0400

    nav bar height is changed to be under navbar. fixed close button for the navbar to be positioned correctly. The texts in navbar highlight white and bold determining on the location you are on

--- Frontend/src/Components/Navbar/Navbar.jsx
+++ Frontend/src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, NavLink } from "react-router-dom";
 import { slide as Menu } from "react-burger-menu";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
@@ -15,14 +15,22 @@ function Navbar() {
       </div>
       <div className="burgermenu">
         <Menu right>
-          <Link to="/home" className="burgermenu-link">Home</Link>
+        <NavLink exact to="/home" activeClassName="active-link" className={`burgermenu-link ${location.pathname === "/home" ? "active-link" : ""}`}>Home</NavLink>
+          <NavLink to="/alphabetdir" activeClassName="active-link" className={`burgermenu-link ${location.pathname === "/alphabetdir" ? "active-link" : ""}`}>Alphabet</NavLink>
+          <NavLink to="/numberdir" activeClassName="active-link" className={`burgermenu-link ${location.pathname === "/numberdir" ? "active-link" : ""}`}>Numbers</NavLink>
+          <NavLink to="/combodir" activeClassName="active-link" className={`burgermenu-link ${location.pathname === "/combodir" ? "active-link" : ""}`}>Combos</NavLink>
+          <NavLink to="/specialchardir" activeClassName="active-link" className={`burgermenu-link ${location.pathname === "/specialchardir" ? "active-link" : ""}`}>Special Characters</NavLink>
+          <NavLink to="/phrases" activeClassName="active-link" className={`burgermenu-link ${location.pathname === "/phrases" ? "active-link" : ""}`}>Phrases</NavLink>
+          <NavLink to="/quiz" activeClassName="active-link" className={`burgermenu-link ${location.pathname === "/quiz" ? "active-link" : ""}`}>Quizes</NavLink>
+          <NavLink to="/devteam" activeClassName="active-link" className={`burgermenu-link ${location.pathname === "/devteam" ? "active-link" : ""}`}>About Us</NavLink>
+          {/* <Link to="/home" className="burgermenu-link">Home</Link>
           <Link to="/alphabetdir" className="burgermenu-link">Alphabet</Link>
           <Link to="/numberdir" className="burgermenu-link">Numbers</Link>
           <Link to="/combodir" className="burgermenu-link">Combos</Link>
           <Link to="/specialchardir" className="burgermenu-link">Special Characters</Link>
           <Link to="/phrases" className="burgermenu-link">Phrases</Link>
           <Link to ="/quiz" className="burgermenu-link">Quizes</Link>
-          <Link to='/devteam' className="burgermenu-link">About Us</Link>
+          <Link to='/devteam' className="burgermenu-link">About Us</Link> */}
         </Menu>
       </div>
     </div>
